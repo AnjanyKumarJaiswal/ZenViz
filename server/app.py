eace94e6225ff683deab3ab858c118fb9b250cd0
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2025-03-26 21:09:57 +0530

    feat: routes added for login

--- server/app.py
+++ server/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
+from database.db import initialised_DB
+from api.services.auth import user_login
 
 app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/')
+initialised_DB(app=app)
+
+@app.route('/api/')
 def testing():
     return "<a>hello this is working really fine<a/>"
+
+@app.route('/api/auth/login', methods=['POST'])
+def login():
+    return user_login()
 
 if __name__ == "__main__":
     app.run(debug=True)
