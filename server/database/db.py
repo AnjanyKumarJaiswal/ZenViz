11dbbd4a2b8ea1d3223c404f8b0ca028b86be0d6
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2025-03-26 21:01:13 +0530

    feat: mysql database setup

---
+++ server/database/db.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+def initialised_DB(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = mysql_config
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
+    db.init_app(app)
+    Migrate(app, db)
+    
+    # try:
+    #     # print("Initialising SQL Database....")
+    #     db.init_app(app)
+    #     # print("Now Initialising Migration of the Database...")
+    #     Migrate(app, db)
+    #     # print("Migrattion Done....")
+    # except:
+    #     print("An Error Occured While Initialising Database")
