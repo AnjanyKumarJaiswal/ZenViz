4b4c8914f565069e7334ef6e5fc3bf9c7067b7c2
AnjanyKumarJaiswal <136046942+AnjanyKumarJaiswal@users.noreply.github.com>
2025-06-03 17:22:02 +0530

    fix: test 3 on solving cors issue

--- server/app.py
+++ server/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(
     app,
     supports_credentials=True,
-    origins="https://zen-viz.vercel.app/",
+    origins="https://zen-viz.vercel.app",
     allow_headers=["Content-Type", "Authorization"]
     )
 
